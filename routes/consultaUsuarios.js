83de9c377ac1b193ea8e0689c9e3372bd9ee7e10
KevinFrutos <vinke.14297@gmail.com>
2021-11-25 14:38:55 +0100

    Intentando mejorar el sistema de consulta de usuarios con usando async/await

--- routes/consultaUsuarios.js
+++ routes/consultaUsuarios.js
@@ -2,11 +2,21 @@ const router = require('express').Router();
 const db = require('../database');
 
 router.get('/usuarios', async (req, res) => {
-    db.collection('registros').find().toArray()
+  try{
+    const cursor = await db.collection('registros').find().toArray()
+    cursor.forEach(item => {
+      res.send(item)
+    })
+  }catch(err){
+    console.log(err)
+  }
+})
+
+module.exports = router
+/*
+db.collection('registros').find().toArray()
     .then(results => {
       res.send(results)
     })
     .catch(error => console.error(error))
-})
-
-module.exports = router
+     */
