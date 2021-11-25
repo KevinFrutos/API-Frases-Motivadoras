882d4c747fdc779b0d08cff7d662fbc1a05fe777
KevinFrutos <vinke.14297@gmail.com>
2021-11-26 00:46:34 +0100

    He instalado la dependencia bcryptjs para encriptar los password antes de enviarlos a la base de datos

--- database.js
+++ database.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 const url = "mongodb+srv://" + process.env.USER + ":" + process.env.PASSWORD + "@api-general.xdns4.mongodb.net/" + process.env.DBNAME + "?retryWrites=true&w=majority";
 
-const db = mongoose.createConnection(url, 
-  { useNewUrlParser: true, useUnifiedTopology: true }
-)
+const db = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true })
 db.on('error', (err) => {
   reject(err);
  });
