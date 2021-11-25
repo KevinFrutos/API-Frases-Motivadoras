87a6a1700be64fdd1fc2e06eaead567b6fac5d14
KevinFrutos <vinke.14297@gmail.com>
2021-11-25 22:55:33 +0100

    Al final he quitado las variables de entorno de aws-sdk

--- database.js
+++ database.js
@@ -1,14 +1,6 @@
 const mongoose = require('mongoose');
-// AWS-SDK ES PARA QUE LA APP FUNCIONE EN HEROKU
-const aws = require('aws-sdk');
 
-let s3 = new aws.S3({
-  USER: process.env.USER,
-  PASSWORD: process.env.PASSWORD,
-  DBNAME: process.env.DBNAME
-});
-
-const url = "mongodb+srv://" + s3.USER + ":" + s3.PASSWORD + "@api-general.xdns4.mongodb.net/" + s3.DBNAME + "?retryWrites=true&w=majority";
+const url = "mongodb+srv://" + process.env.USER + ":" + process.env.PASSWORD + "@api-general.xdns4.mongodb.net/" + process.env.DBNAME + "?retryWrites=true&w=majority";
 
 const db = mongoose.createConnection(url, 
   { useNewUrlParser: true, useUnifiedTopology: true }
