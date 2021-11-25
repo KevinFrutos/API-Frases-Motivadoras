09813c6b9dd471c29e334605750bde56a3a2bf1e
KevinFrutos <vinke.14297@gmail.com>
2021-11-25 01:08:20 +0100

    He instalado la libreria express-validator para validar los registros y he completado el schema de registro ya valida

--- models/schema_registrarse.js
+++ models/schema_registrarse.js
@@ -2,8 +2,11 @@ const { Schema } = require("mongoose");
 const db = require('../database');
 
 const schema = new Schema({
-    nombre: { type: String, min: 3, max: 50 },
-    email: { type: String, unique: true}
+    nombre_usuario: { type: String, required: true, unique: true },
+    nombre: { type: String, required: true },
+    apellido: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    passwd: { type: String, required: true }
 });
 
 module.exports = db.model('Registro', schema)
--- routes/registrarse.js
+++ routes/registrarse.js
@@ -1,22 +1,32 @@
 const router = require('express').Router();
 const Registrarse = require('../models/schema_registrarse')
+const { body, validationResult } = require('express-validator'); // USO ESTA LIBRERIA PARA VALIDAR LOS DATOS ANTES DE GUARDARLOS.
 
-router.post('/registrarse', async (req, res) => {
-    console.log(req.body.nombre)
-    console.log(req.body.email);
-    const registrarse = new Registrarse({
-        nombre: req.body.nombre,
-        email: req.body.email
-    })
-    registrarse.save(err => {
-        if (err) {
-            console.log(err);
+router.post('/registrarse',
+    body('nombre_usuario').isLength({ min: 6 }),
+    body('email').isEmail(),
+    body('passwd').isLength({ min: 6 }),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-    });
-    res.json({
-        error: null,
-        data: 'funciona todo ok!'
+        const registrarse = new Registrarse({
+            nombre_usuario: req.body.nombre_usuario,
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            email: req.body.email,
+            passwd: req.body.passwd
+        })
+        registrarse.save(err => {
+            if (err) {
+                console.log(err);
+            }
+        });
+        res.json({
+            error: null,
+            data: 'funciona todo ok!'
+        })
     })
-})
 
 module.exports = router;
