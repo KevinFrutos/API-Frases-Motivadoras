30841a3a9b6991e7eb7eba29b70f3ebca088222c
KevinFrutos <vinke.14297@gmail.com>
2021-11-26 01:16:34 +0100

    He utilizado bcryptjs para encriptar las contraseñas antes de subirlas a la base de datos

---
+++ routes/login.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+router.post('/login', (req,res) => {
+    res.send("holita?")
+})
+
+module.exports = router;
+
+/*
+req.body.passwd: aqui va la contraseña que el usuario introduce en el login
+hash: aqui va la contraseña encriptada que esta guardada en la base de datos, asociada al nombre de Usuario
+bcrypt.compare(req.body.passwd, hash, function(err, res) {
+                    console.log(res)
+                });
+
+*/
--- routes/registrarse.js
+++ routes/registrarse.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Registrarse = require('../models/schema_registrarse')
 const { body, validationResult } = require('express-validator'); // USO ESTA LIBRERIA PARA VALIDAR LOS DATOS ANTES DE GUARDARLOS.
+const bcrypt = require("bcryptjs"); // USO ESTA LIBRERIA PARA ENCRIPTAR LA CONTRASEÑA ANTES DE ENVIARLA A LA BASE DE DATOS
 
 router.post('/registrarse',
     // UTILIZO EXPRESS-VALIDATOR PARA VALIDAR LOS CAMPOS SIGUIENTES
@@ -14,29 +15,33 @@ router.post('/registrarse',
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        // EN CASO DE QUE LOS DATOS ESTEN CORRECTOS SE UTILIZA EL SCHEMA QUE HE CREADO PARA LA INSERCION DE DATOS EN LA BASE DE DATOS
-        // Y SE RELLENAN LOS CAMPOS CON LOS DATOS OBTENIDOS DEL FORMULARIO.
-        const registrarse = new Registrarse({
-            nombre_usuario: req.body.nombre_usuario,
-            nombre: req.body.nombre,
-            apellido: req.body.apellido,
-            email: req.body.email,
-            passwd: req.body.passwd
-        })
-        // SE ENVIA EL OBJETO JSON A LA BASE DE DATOS Y SE CREA LA ENTRADA CON LOS DATOS ENVIADOS.
-        registrarse.save(err => {
-            if (err) {
-                console.log(err);
-                res.json({
-                    error: err,
-                    data: 'Algo fue mal'
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(req.body.passwd, salt, function(err, hash) {
+                // EN CASO DE QUE LOS DATOS ESTEN CORRECTOS SE UTILIZA EL SCHEMA QUE HE CREADO PARA LA INSERCION DE DATOS EN LA BASE DE DATOS
+                // Y SE RELLENAN LOS CAMPOS CON LOS DATOS OBTENIDOS DEL FORMULARIO.
+                const registrarse = new Registrarse({
+                    nombre_usuario: req.body.nombre_usuario,
+                    nombre: req.body.nombre,
+                    apellido: req.body.apellido,
+                    email: req.body.email,
+                    passwd: hash
                 })
-            }else{
-                res.json({
-                    error: null,
-                    data: 'Conexion correcta'
-                })
-            }
+                // SE ENVIA EL OBJETO JSON A LA BASE DE DATOS Y SE CREA LA ENTRADA CON LOS DATOS ENVIADOS.
+                registrarse.save(err => {
+                    if (err) {
+                        console.log(err);
+                        res.json({
+                            error: err,
+                            data: 'Algo fue mal'
+                        })
+                    }else{
+                        res.json({
+                            error: null,
+                            data: 'Conexion correcta'
+                        })
+                    }
+                });
+            });
         });
     })
 
