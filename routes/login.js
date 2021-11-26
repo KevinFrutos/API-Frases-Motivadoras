a3dd20947a26b2115443a5a07eb56ca0700bc7ea
KevinFrutos <vinke.14297@gmail.com>
2021-11-26 02:23:10 +0100

    Comentando el archivo de la ruta Log-In

--- routes/login.js
+++ routes/login.js
@@ -4,13 +4,24 @@ const bcrypt = require("bcryptjs"); // CON ESTA LIBRERIA COMPARO LA CONTRASEÑA 
 
 router.post('/login', async (req,res) => {
     try{
+        // BUSCO SI EXISTE ALGUN USUARIO EN LA BASE DE DATOS CON EL NOMBRE QUE ME HAN PROPORCONADO
+        // EN CASO DE QUE EXISTE ME ENVIA SUS DATOS
         const cursor = await db.collection('registros').find({nombre_usuario: req.body.nombre_usuario}).toArray()
+        // UTILIZO EL HASH DE CONTRASEÑA QUE ME HA ENVIADO LA BASE DE DATOS 
+        // Y LA COMPARO CON LA CONTRASEÑA QUE EL USUARIO HA INTRODUCIDO
         bcrypt.compare(req.body.passwd, cursor[0].passwd, function(err, resultado) {
-            //console.log(resultado)
-            res.json({
-                error: null,
-                data: 'Log-In realizado correctamente'
-            })
+            //console.log(resultado) // TRUE SI EL PASSWORD ES CORRECTO
+            if(err){
+                res.json({
+                    error: err,
+                    data: 'Algo fue mal'
+                })
+            }else{
+                res.json({
+                    error: null,
+                    data: 'Log-In realizado correctamente'
+                })
+            }
         });
       }catch(err){
         console.log(err)
