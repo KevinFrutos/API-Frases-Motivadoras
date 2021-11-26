f164746902f478f29b3848b24a163628e13369f7
KevinFrutos <vinke.14297@gmail.com>
2021-11-26 13:39:18 +0100

    He cambiado la ruta consultas a consulta y he agregado el parametro ?nombre_usuario=, ademas he actualizado el archivo rutas.txt

--- routes/consultaUsuarios.js
+++ routes/consultaUsuarios.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const db = require('../database');
 
-router.get('/usuarios', async (req, res) => {
+router.get('/usuario', async (req, res) => {
   try{
-    const cursor = await db.collection('registros').find().toArray()
+    // COMPRUEBO SI HAY ALGUN USUARIO REGISTRADO CON EL NOMBRE DE USUARIO PROPORCIONADO
+    const cursor = await db.collection('registros').find({nombre_usuario: req.query.nombre_usuario}).toArray()
+    // EN CASO DE QUE EL USUARIO EXISTA, SE ENVIAN LOS DATOS
     res.send(cursor)
   }catch(err){
     console.log(err)
