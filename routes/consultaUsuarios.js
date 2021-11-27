b669a576bda38d1d213aa77ce396d7f36f09807f
KevinFrutos <vinke.14297@gmail.com>
2021-11-27 14:09:24 +0100

    Cuando el usuario consulta sus datos, solo se le envia: usuario, nombre, apellido e email

--- routes/consultaUsuarios.js
+++ routes/consultaUsuarios.js
@@ -2,21 +2,26 @@ const router = require('express').Router();
 const db = require('../database');
 
 router.get('/usuario', async (req, res) => {
-  try{
-    if(req.token.nombre_usuario == req.query.nombre_usuario){
+  try {
+    if (req.token.nombre_usuario == req.query.nombre_usuario) {
       // COMPRUEBO SI HAY ALGUN USUARIO REGISTRADO CON EL NOMBRE DE USUARIO PROPORCIONADO
-      const cursor = await db.collection('registros').findOne({nombre_usuario: req.query.nombre_usuario})
+      const cursor = await db.collection('registros').findOne({ nombre_usuario: req.query.nombre_usuario })
       // LA SIGUIENTE LINEA ME DA ESTE ERROR: Error: Collection method find is synchronous
       // POR ESA RAZON HE CAMBIADO EL find() POR findOne() Y HE QUITADO EL toArray()
       // const cursor = await db.collection('registros').find({nombre_usuario: req.query.nombre_usuario}).toArray()
-      
+
       // EN CASO DE QUE EL USUARIO EXISTA, SE ENVIAN LOS DATOS
-      res.status(200).send(cursor)
-    }else{
+      res.status(200).json({
+        nombre_usuario: cursor.nombre_usuario,
+        nombre: cursor.nombre,
+        apellido: cursor.apellido,
+        email: cursor.email,
+      })
+    } else {
       res.status(400).json({ error: 'token no es válido' })
     }
 
-  }catch(err){
+  } catch (err) {
     console.log(err)
   }
 })
