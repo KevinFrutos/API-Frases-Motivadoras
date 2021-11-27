b1e5a270855e18703ccae00d7b8056bc4e11bd6e
KevinFrutos <vinke.14297@gmail.com>
2021-11-27 14:15:37 +0100

    Variables de entorno

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = "mongodb+srv://" + process.env.USER + ":" + process.env.PASSWORD + "@api-general.xdns4.mongodb.net/" + process.env.DBNAME + "?retryWrites=true&w=majority";
+const url = "mongodb+srv://" + process.env.USER + ":" + process.env.PASSWORD + "@" + process.env.HOST + "/" + process.env.DBNAME + "?retryWrites=true&w=majority";
 
 const db = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true })
 db.on('error', (err) => {
