bb9b2eede1a3015f0233fc8b919b6454ae4b020c
KevinFrutos <vinke.14297@gmail.com>
2021-11-28 12:15:01 +0100

    He quitado que el usuario tenga que enviar el usuario cuando agrega una frase

--- routes/agregarFrase.js
+++ routes/agregarFrase.js
@@ -6,48 +6,39 @@ router.post('/frase',
     // UTILIZO EXPRESS-VALIDATOR PARA VALIDAR LOS CAMPOS SIGUIENTES
     body('frase').isLength({ min: 6 }),
     (req, res) => {
-        const usuario = req.header('nombre_usuario')
-        if(req.token.nombre_usuario === usuario){
-
-            // USO EL METODO VALIDATIONRESULT PARA CAPTURAR LOS POSIBLES ERRORES EN LA VALIDACION DE CAMPOS
-            const errors = validationResult(req);
-            // EN CASO DE CAPTURAR ALGUN ERROR, SE DEVUELVE UN STATUS 400 Y UN JSON CON EL ERROR Y EL CAMPO AFECTADO.
-            if (!errors.isEmpty()) {
-                return res.status(400).json({ errors: errors.array() });
-            }
-            let frase = req.body.frase
-            let autor = ""
-            if(req.body.autor){
-                autor = req.body.autor
-            }
-            // EN CASO DE QUE LOS DATOS ESTEN CORRECTOS SE UTILIZA EL SCHEMA QUE HE CREADO PARA LA INSERCION DE DATOS EN LA BASE DE DATOS
-            // Y SE RELLENAN LOS CAMPOS CON LOS DATOS OBTENIDOS DEL FORMULARIO.
-            const insertar_frase = new Frases({
-                frase: frase,
-                autor: autor
-            })
-            // SE ENVIA EL OBJETO JSON A LA BASE DE DATOS Y SE CREA LA ENTRADA CON LOS DATOS ENVIADOS.
-            insertar_frase.save(err => {
-                if (err) {
-                    console.log(err);
-                    res.status(400).json({
-                        error: err,
-                        data: 'Algo fue mal'
-                    })
-                } else {
-                    // EN CASO QUE EL USUARIO SEA CORRECTO Y SE GUARDE EN LA BASE DE DATOS SE ENVIA EL TOKEN
-                    res.status(200).json({
-                        error: null,
-                        description: "Envio realizado correctamente"
-                    })
-                }
-            });
-        }else{
-            res.status(400).json({
-                error: err,
-                data: 'Algo fue mal'
-            })
+        // USO EL METODO VALIDATIONRESULT PARA CAPTURAR LOS POSIBLES ERRORES EN LA VALIDACION DE CAMPOS
+        const errors = validationResult(req);
+        // EN CASO DE CAPTURAR ALGUN ERROR, SE DEVUELVE UN STATUS 400 Y UN JSON CON EL ERROR Y EL CAMPO AFECTADO.
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
+        let frase = req.body.frase
+        let autor = ""
+        if (req.body.autor) {
+            autor = req.body.autor
+        }
+        // EN CASO DE QUE LOS DATOS ESTEN CORRECTOS SE UTILIZA EL SCHEMA QUE HE CREADO PARA LA INSERCION DE DATOS EN LA BASE DE DATOS
+        // Y SE RELLENAN LOS CAMPOS CON LOS DATOS OBTENIDOS DEL FORMULARIO.
+        const insertar_frase = new Frases({
+            frase: frase,
+            autor: autor
+        })
+        // SE ENVIA EL OBJETO JSON A LA BASE DE DATOS Y SE CREA LA ENTRADA CON LOS DATOS ENVIADOS.
+        insertar_frase.save(err => {
+            if (err) {
+                console.log(err);
+                res.status(400).json({
+                    error: err,
+                    data: 'Algo fue mal'
+                })
+            } else {
+                // EN CASO QUE EL USUARIO SEA CORRECTO Y SE GUARDE EN LA BASE DE DATOS SE ENVIA EL TOKEN
+                res.status(200).json({
+                    error: null,
+                    description: "Envio realizado correctamente"
+                })
+            }
+        });
     })
 
 module.exports = router;
